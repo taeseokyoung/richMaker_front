059b6c8ffb36d8ebb612c38ab015fcd2910d1479
SungSuSon <thstjdtn05@gmail.com>
2023-05-27 22:09:27 +0900

    :sparkles:Feat: create Challenge Detail Fetch API
    상세페이지 API 연결 완

--- js/challenge-detail.js
+++ js/challenge-detail.js
@@ -1,13 +1,37 @@
-// import { BACK_BASE_URL, FRONT_BASE_URL } from "./conf.js";
-const BACK_BASE_URL = "http://127.0.0.1:8000";
-const FRONT_BASE_URL = "http://127.0.0.1:5500";
+import { BACK_BASE_URL, FRONT_BASE_URL } from "./conf.js";
+import { challengeLikeAPI, challengeBookmarkAPI } from "./api.js";
 
-handleChallenge()
 
-async function handleChallenge() {
+
+
+export async function payloadParse() {
+    const payload = localStorage.getItem("payload");
+    const payload_parse = JSON.parse(payload)
+    return payload_parse
+}
+
+
+export async function getChallengeId() {
     const urlParams = new URLSearchParams(window.location.search);
     const challengeId = urlParams.get('challenge_id')
+    return challengeId
+}
 
+
+// 챌린지 북마크 등록, 취소하기
+document.getElementById("challengeBookmarkButton").addEventListener("click", challengeBookmark)
+// // 챌린지 좋아요(관심) 등록, 취소하기
+document.getElementById("challengeLikeButton").addEventListener("click", challengeLike)
+
+
+// 해당 챌린지에 참여중인(북마크 등록한) 유저 리스트 뽑아오기
+document.getElementById("showBookmarkingList").addEventListener("click", showBookmarkingList);
+// 해당 챌린지에 관심등록(좋아요 누른) 유저 리스트 뽑아오기
+document.getElementById("showLikingList").addEventListener("click", showLikingList);
+
+
+export async function handleChallenge() {
+    const challengeId = await getChallengeId()
     const response = await fetch(`${BACK_BASE_URL}/api/challenge/${challengeId}`,)
 
     const data = await response.json();
@@ -16,8 +40,8 @@ async function handleChallenge() {
     document.querySelector('#period').value = data.period
     document.querySelector('#amount').value = data.amount
     document.querySelector('.note-editable p').innerHTML = data.challenge_content
-    console.log(data)
 }
+handleChallenge()
 
 
 
@@ -30,72 +54,108 @@ async function handleChallenge() {
 
 
 
-// 챌린지 북마크 등록, 취소하기
-// document.getElementById("bookmark_button").addEventListener("click", handleSignup);
-// // 챌린지 좋아요(관심) 등록, 취소하기
-// document.getElementById("like_button").addEventListener("click", EmailAuthentication);
+//  사용자가 챌린지 좋아요 등록 및 취소
+export async function challengeLike() {
+    const challengeId = await getChallengeId()
+    const response = await challengeLikeAPI(challengeId)
+    try {
+        const response_json = await response.json()
+        console.log(response.status)
+        if (response.status == 204) {
+            // 챌린지 좋아요 취소
+            console.log(response_json)
+        } else if (response.status == 201) {
+            // 챌린지 좋아요 등록
+            console.log(response_json)
+        } else {
+            // 로그인 필요 또는 찾을 수 없는 챌린지
+            console.log(response_json)
+            const payload_parse = await payloadParse()
+            if (payload_parse == null) {
+                alert("로그인이 필요합니다.")
+                window.location.replace(`${FRONT_BASE_URL}/login.html`);
+            } else {
+                alert("챌린지 게시글을 찾을 수 없습니다.")
+                window.location.replace(`${FRONT_BASE_URL}/index.html`);
+            }
 
-// // 해당 챌린지에 참여중인(북마크 등록한) 유저 리스트 뽑아오기
-// document.getElementById("show_bookmarkingList_button").addEventListener("click", handleSignup);
-// // 해당 챌린지에 관심등록(좋아요 누른) 유저 리스트 뽑아오기
-// document.getElementById("show_likeingList_button").addEventListener("click", EmailAuthentication);
+        }
+    } catch (error) {
+        // 챌린지 좋아요 취소, 비동기 에러
+        console.log("챌린지 좋아요 취소")
+    }
+}
+
+
+// 사용자가 챌린지 북마크 등록 및 취소
+export async function challengeBookmark() {
+    const challengeId = await getChallengeId()
+    const response = await challengeBookmarkAPI(challengeId)
+    try {
+        const response_json = await response.json()
+        if (response.status == 204) {
+            // 챌린지 북마크 취소
+            console.log(response_json)
+        } else if (response.status == 201) {
+            // 챌린지 북마크 등록
+            console.log(response_json)
+        } else {
+            // 로그인 필요 또는 찾을 수 없는 챌린지
+            console.log(response_json)
+
+            const payload_parse = await payloadParse()
+            if (payload_parse == null) {
+                alert("로그인이 필요합니다.")
+                window.location.replace(`${FRONT_BASE_URL}/login.html`);
+            } else {
+                alert("챌린지 게시글을 찾을 수 없습니다.")
+                window.location.replace(`${FRONT_BASE_URL}/index.html`);
+            }
+
+        }
+    } catch (error) {
+        // 챌린지 북마크 취소 비동기 에러
+        console.log("북마크 등록 취소")
+    }
+}
 
 
 
-// 사용자가 챌린지 좋아요 등록 및 취소
-async function challengeLikeAPI(challenge_id) {
-    const access_token = localStorage.getItem("access")
-    const response = await fetch(`${BACK_BASE_URL}/api/users/likes/${user_id}/`, {
-        headers: {
-            'content-type': 'application/json',
-            "Authorization": `Bearer ${access_token}`,
-        },
-        method: 'POST',
-    })
+
+
+
+//  챌린지 관심 등록한 유저 리스트 뽑아오기
+export async function showLikingListAPI(user_id) {
+    const response = await fetch(`${BACK_BASE_URL}/api/users/likes/${user_id}/`)
+    return response
+
+}
+//  챌린지 북마크 등록한 유저 리스트 뽑아오기
+export async function showBookmarkingListAPI(user_id) {
+    const response = await fetch(`${BACK_BASE_URL}/api/users/bookmark/${user_id}/`)
     return response
 }
-async function challengeLike() {
 
-    const urlParams = new URLSearchParams(window.location.search);
-    let challenge_id = urlParams.get('challenge_id');
-    console.log(challenge_id)
 
-    // const response = await challengeLikeAPI(challenge_id)
-    // const response_json = await response.json()
-    // if (response.status == 204) {
-    //     // 챌린지 좋아요 취소
-    // } else if (response.status == 201) {
-    //     // 챌린지 좋아요 등록
-    // }else{
-    //     // 로그인 필요 또는 찾을 수 없는 챌린지
-    // }
+
+export async function showBookmarkingList() {
+    const challengeId = await getChallengeId()
+    const response = await showBookmarkingListAPI(challengeId)
+    const response_json = await response.json()
+    if (response.status == 200) {
+        console.log(response_json)
+    } else {
+        console.log(response_json)
+    }
 }
+export async function showLikingList() {
+    const challengeId = await getChallengeId()
+    const response = await showLikingListAPI(challengeId)
+    const response_json = await response.json()
+    if (response.status == 200) {
+        console.log(response_json)
+    } else {
+        console.log(response_json)
+    }
 
-
-
-// export async function challengeBookmarkAPI(user_id) {
-//     const access_token = localStorage.getItem("access")
-//     const response = await fetch(`${BACK_BASE_URL}/api/users/bookmark/${user_id}/`, {
-//         headers: {
-//             'content-type': 'application/json',
-//             "Authorization": `Bearer ${access_token}`,
-//         },
-//         method: 'POST',
-//     })
-//     return response
-// }
-
-
-
-// //  챌린지 관심 등록한 유저 리스트 뽑아오기
-// export async function showLikingListAPI(user_id) {
-//     const response = await fetch(`${BACK_BASE_URL}/api/users/likes/${user_id}/`)
-//     return response
-
-// }
-// //  챌린지 북마크 등록한 유저 리스트 뽑아오기
-// export async function showBookmarkingListAPI(user_id) {
-//     const response = await fetch(`${BACK_BASE_URL}/api/users/bookmark/${user_id}/`)
-//     return response
-// }
-
+}
